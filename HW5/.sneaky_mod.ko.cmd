39ec7fdb108f945ed50791a4ac019795a2ee5f76
KlopTim <731954402@qq.com>
2022-04-08 20:45:55 -0400

    necessary file

--- HW5/.sneaky_mod.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/xh110/650/ECE650/HW5/sneaky_mod.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/xh110/650/ECE650/HW5/sneaky_mod.ko /home/xh110/650/ECE650/HW5/sneaky_mod.o /home/xh110/650/ECE650/HW5/sneaky_mod.mod.o;  true
--- HW5/.sneaky_mod.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/xh110/650/ECE650/HW5/sneaky_mod.mod := { echo  /home/xh110/650/ECE650/HW5/sneaky_mod.o;  echo; } > /home/xh110/650/ECE650/HW5/sneaky_mod.mod
